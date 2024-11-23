67d1ed8f2faeff283bc31b56a535549cc7ea2169
evergara <emersonjb@gmail.com>
2024-11-23 05:58:52 -0500

    first commit base project

---
+++ src/test/java/com/has/app/musictify/MusictifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.has.app.musictify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusictifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
